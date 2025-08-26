2687b65141d87c951a78cbd304058e1331702d1d
ori.ml <orimenachemlevi@gmail.com>
2025-08-26 21:21:54 +0300

    # fix envs

--- client/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://jamoveo-api.onrender.com',
-  socketUrl: 'https://jamoveo-api.onrender.com/io'
-};
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-  socketUrl: 'https://www.jamoveo-api.onrender.com'
+  production: true,
+  apiUrl: 'https://jamoveo-api.onrender.com',
+  socketUrl: 'https://jamoveo-api.onrender.com'
 };
