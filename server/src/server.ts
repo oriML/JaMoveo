705fddfb24585e1d3e37a98d80647a4cffb65712
ori.ml <orimenachemlevi@gmail.com>
2025-08-26 22:34:54 +0300

    # remove CORS after static change

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,12 +16,12 @@ import path from 'path';
 const app = express();
 const server = http.createServer(app);
 
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN || 'http://localhost:4200', // Allow CORS from specified origin or default
-    credentials: true,
-};
+// const corsOptions = {
+//     origin: process.env.CORS_ORIGIN || 'http://localhost:4200', // Allow CORS from specified origin or default
+//     credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -41,7 +41,7 @@ app.use('/api/live', liveRoutes);
 
 
 const io = new Server(server, {
-    cors: corsOptions
+    // cors: corsOptions
 });
 
 app.set('io', io);
@@ -52,7 +52,7 @@ const publicPath = path.join(__dirname, "public", "browser");
 app.use(express.static(publicPath));
 
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+    res.sendFile(path.join(publicPath, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
